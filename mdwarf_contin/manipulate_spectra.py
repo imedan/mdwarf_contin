5a23641a8e6bccefc73c775cfaea4b1a82414447
imedan <ilija.medan@gmail.com>
2024-03-20 11:24:17 -0500

    add function to fully manipulate model specrtra

--- mdwarf_contin/manipulate_spectra.py
+++ mdwarf_contin/manipulate_spectra.py
@@ -3,6 +3,9 @@ import numpy as np
 import astropy.units as u
 from importlib.resources import open_binary
 import pickle
+from scipy.ndimage import gaussian_filter1d
+from scipy.interpolate import interp1d
+import scipy.stats as ss
 
 
 # get PCA data
@@ -66,3 +69,54 @@ def random_response(loglam: np.ndarray, flux: np.ndarray):
     med = np.nanmedian(flux[mask])
     flux_resp = (flux / med + resp) * med
     return flux_resp
+
+
+def manipulate_model_spectra(loglam_sdss: np.ndarray,
+                             loglam_model: np.ndarray,
+                             flux_model: np.ndarray,
+                             size: int):
+    """
+    Manipulate a model spectrum by by smoothing, downsampling
+    adding reddening and instrument response
+
+    Parameters
+    ----------
+    loglam_sdss: np.array
+        loglam from sdss spectrum
+
+    loglam_model: np.array
+        loglam of the model spectrum
+
+    flux_model: np.array
+        flux of the model spectrum
+
+    size: int
+        number of random spectra to return
+
+    Returns
+    -------
+    flux_rand: np.array
+        Flux of the random manipulated model spectra.
+        Flux is evaluated at loglam_sdss. Size of array will be
+        (size, len(loglam_sdss)).
+    """
+    # smooth and downsample the spectrum
+    flux_smooth = gaussian_filter1d(flux_model,
+                                    (10 ** loglam_sdss[1] - 10 ** loglam_sdss[0]) /
+                                    (10 ** loglam_model[1] - 10 ** loglam_model[0]) / 3)
+    f_model = interp1d(loglam_model, flux_smooth)
+    flux_smooth_down = f_model(loglam_sdss)
+
+    flux_rand = np.zeros((size, len(loglam_sdss)))
+
+    # add redenning to the spectra
+    P = np.array([1.5402553, -0.0009273592438195921, 0.27507633])  # fit to 1 kpc M dwarfs
+    av_rand = ss.lognorm.rvs(*P, size=size)
+    for i in range(size):
+        flux_rand[i, :] = add_reddening(loglam_sdss, flux_rand[i, :], av_rand[i])
+
+    # add the instrument response
+    for i in range(size):
+        flux_rand[i, :] = random_response(loglam_sdss, flux_smooth_down)
+
+    return flux_rand
